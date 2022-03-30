aea371f01fc2deeac9b7e5d65a365e415629f718
xsinus <fun1ham@gmail.com>
2022-03-30 10:11:48 +0300

    added category kb

--- games/bunny.py
+++ games/bunny.py
@@ -1,14 +1,25 @@
-import bunny_lists as bl
-
-games = {}
+from bunny_lists import *
+from game_instance import *
+from telebot import types
 
 
 def on_message(bot, message):
     if 'group' not in message.chat.type:
         return
     contentArr = message.text.split()[1:]
-    if contentArr and contentArr[0] == 'reg':
+    cmd = contentArr[0]
+    if contentArr and cmd == 'reg':
         try:
             bot.send_message(message.from_user.id, 'hello')
         except Exception:
             bot.send_message(message.chat.id, 'Сначала начните со мной диалог')
+    if contentArr and cmd == 'start':
+        keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
+
+        for cat in lists:
+            cat_key = types.InlineKeyboardButton(
+                text=cat, callback_data=cat)
+            keyboard.add(cat_key)
+
+        bot.send_message(message.chat.id, text='Выберете категорию:',
+                         reply_markup=keyboard)
