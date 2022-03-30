8e56a6d26de08937b7d76071e8f4a65b61862cee
xsinus <fun1ham@gmail.com>
2022-03-30 12:43:13 +0300

    added word and bunny choosing

--- games/bunny.py
+++ games/bunny.py
@@ -1,20 +1,30 @@
-from bunny_lists import *
-from game_instance import *
+from matplotlib.pyplot import text
 from telebot import types
+import random
+from .bunny_lists import *
+from .game_instance import game_instance
+from copy import deepcopy
+
+sessions = {}
 
 
 def on_message(bot, message):
-    if 'group' not in message.chat.type:
-        return
     contentArr = message.text.split()[1:]
+    if 'group' not in message.chat.type or not len(contentArr):
+        return
     cmd = contentArr[0]
-    if contentArr and cmd == 'reg':
+    chat_id = message.chat.id
+    if chat_id not in sessions:
+        sessions[chat_id] = deepcopy(
+            game_instance)
+    if cmd == 'reg':
         try:
             bot.send_message(message.from_user.id, 'Вы были зарегестрированы')
-            games[message.chat.id]["player_ids"].append(message.from_user.id)
+            sessions[chat_id]["player_ids"].append(
+                message.from_user.id)
         except Exception:
             bot.send_message(message.chat.id, 'Сначала начните со мной диалог')
-    if contentArr and cmd == 'start':
+    if cmd == 'start':
         keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
 
         for cat in lists:
@@ -24,3 +34,29 @@ def on_message(bot, message):
 
         bot.send_message(message.chat.id, text='Выберете категорию:',
                          reply_markup=keyboard)
+
+
+def on_keyboard(bot, call):
+    chat_id = call.message.chat.id
+    if len(sessions[chat_id]['player_ids']) < 3:
+        return bot.send_message(chat_id, text='Мало игроков')
+
+    bot.edit_message_text(chat_id=chat_id, message_id=call.message.id,
+                          text='тема выбрана')
+
+    sample = random.sample(lists[call.data], 16)
+    word = random.choice(sample)
+
+    msg = '-' + '\n-'.join(sample)
+    bot.send_message(chat_id, text=msg)
+
+    print(sessions[chat_id]['player_ids'])
+    player_ids = sessions[chat_id]['player_ids']
+    bunny_id = random.choice(player_ids)
+    sessions[chat_id]['bunny_id'] = bunny_id
+
+    for id in player_ids:
+        if id == bunny_id:
+            bot.send_message(bunny_id, text='Ты заяц :3')
+        else:
+            bot.send_message(id, text=word)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import telebot
 from games import bunny
 
-sessions = {}
-
 bot = telebot.TeleBot('5153566900:AAEnwjIGmi2g1KlRCDow7zTWCBM_hMnpySY')
 
 
@@ -18,6 +16,7 @@ def on_mafia(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    bunny.on_keyboard(call)
+    bunny.on_keyboard(bot, call)
+
 
 bot.infinity_polling()
