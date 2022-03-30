ee548ee415c387216ece726ce60cd019ec8848b5
xsinus <fun1ham@gmail.com>
2022-03-30 13:58:11 +0300

    v1.0

--- games/bunny.py
+++ games/bunny.py
@@ -51,11 +51,15 @@ def see_bunny(bot, call):
     bunny_un = bot.get_chat_member(chat_id, bunny_id).user.first_name
     if (str(p_id) == str(bunny_id)):
         msg = f'Охотники побеждают! {bunny_un} был зайцем!'
+        sticker_id = 'CAACAgIAAxkBAAEEUexiRDY8qsNl5El9nserEwMd2pMvgwACtQIAAj-VzArH2fIsl0SiQiME'
+
     else:
         msg = f'Ой-ой-ой... {p_name} не был зайцем! {bunny_un} остался незамеченным xD'
+        sticker_id = 'CAACAgIAAxkBAAEEUehiRDYw29iLwy7FF1qkk4a6-s9E5wACrQIAAj-VzAofR6oTAVmiYyME'
 
     bot.edit_message_text(
         chat_id=chat_id, message_id=call.message.id, text=msg)
+    bot.send_sticker(chat_id, sticker_id)
     sessions[chat_id]['bunny_id'] = None
 
 
